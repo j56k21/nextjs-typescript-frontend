c0ac48ed85f9968c39c1e077764dd9e5a21871ed
DESKTOP-888POQG\김지웅 <j56k2195@gmail.com>
2024-04-12 23:32:49 +0900

    init

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+
+
+const serverApiUrl = process.env.NEXT_PUBLIC_TEMP === "true" ? "http://localhost:3000" : process.env.SERVER_API_URL || "http://localhost:8080/api";
+const nextConfig = {
+
+    
+    /* config options here */
+  }
+  
+
+export default nextConfig;
